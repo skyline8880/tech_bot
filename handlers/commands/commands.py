05a1728a5f80dd3319780846bfa568112bda6862
skyline8880 <skyline89_88@mail.ru>
2024-08-15 16:52:36 +0300

    debug

--- handlers/commands/commands.py
+++ handlers/commands/commands.py
@@ -25,13 +25,9 @@ router = Router()
 async def dev_command(message: Message) -> None:
     db = Database()
     await db.insert_into_employee_hire(
-        position_id=3,
-        department_id=1,
+        position_id=1,
+        department_id=3,
         phone='79998533965')
-    await db.insert_into_employee_hire(
-        position_id=4,
-        department_id=2,
-        phone='79991124257')
 
 
 @router.message(Command('start'), IsPrivate(), ~IsAuth())
